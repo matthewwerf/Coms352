c225a4904d49062051fd7b58e3c271fcee458ac5
Matthew C Vanderwerf - student <matthewv@pyrite-n1.cs.iastate.edu>
2017-09-21 17:25:06 -0500

    final program

--- hw3q7.c
+++ hw3q7.c
@@ -2,29 +2,33 @@
 #include <stdio.h>
 #include <sys/types.h>
 
-int value = 0;
+extern void *hello(void *);
+extern void *world(void *);
 
-extern void *hello(void);
-extern void *world(void);
+pthread_t tid2;
 
 void main(int argc, char *argv[]){
 	pthread_t tid;
-	pthread_t tid2;
 
 	pthread_attr_t attr;
-
 	pthread_attr_init(&attr);
+
 	pthread_create(&tid, &attr, world, NULL);
 	pthread_create(&tid2, &attr, hello, NULL);
+
 	pthread_join(tid, NULL);
 
 	printf("\n");
 }
 
-void *world(void) {
+void *world(void *param) {
+	pthread_yield(); //yield to ensure tid2 is set, if tid2 is uninitialized pthread_join executes instantly
+	pthread_join(tid2, NULL);
 	printf("world");
+	pthread_exit(0);
 }
 
-void *hello(void) {
+void *hello(void *param) {
 	printf("Hello ");
+	pthread_exit(0);
 }
