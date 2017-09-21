9e44a317a6eb726f34513a73d66e3fec4e853fb0
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-09-21 16:48:53 -0500

    hw3q7

---
+++ hw3q7.c
@@ -0,0 +1,30 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+int value = 0;
+
+extern void hello(void);
+extern void world(void);
+
+void main(int argc, char *argv[]){
+	pthread_t tid;
+	pthread_t tid2;
+
+	pthread_attr_t attr;
+
+	pthread_attr_init(&attr);
+	pthread_create(&tid, &attr, world, NULL);
+	pthread_create(&tid2, &attr, hello, NULL);
+	pthread_join(tid, NULL);
+	
+	printf("\n");
+}
+
+void world(void) {
+	printf("world");
+}
+
+void hello(void) {
+	printf("Hello ");
+}
