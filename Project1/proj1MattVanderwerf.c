feb6e43e4adf9efa7ab18a19c4331b25abe5647d
Matthew C Vanderwerf - student <matthewv@pyrite-n2.cs.iastate.edu>
2017-10-18 00:06:01 -0500

    Large update, more tokenize functionality, non background commands, start background command, start command history

--- Project1/proj1MattVanderwerf.c
+++ Project1/proj1MattVanderwerf.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h> 
+#include <string.h> //strcmp and such
+#include <sys/wait.h> //waitpid
 
 #define MAX_LINE 80 /* The maximum length command */
 
-char **tokenize(char *line);
-void execute(char *raw_in);
+char **tokenize(char *line, int *is_background);
+void execute(char *raw_in, int *should_run);
+void update_command_history(char tokens_history[][MAX_LINE], char last_command[]);
 
 int main(void) {
 	char *args[MAX_LINE/2 + 1]; /* command line arguments */
 	int should_run = 1; /* flag to determine when to exit program */
+	char tokens_history[10][MAX_LINE];
+	int history_count = 0;
 
 	while (should_run) {
 		printf("osh>");
@@ -31,17 +35,31 @@ int main(void) {
 		//debug input
 		//printf("%s", raw_input);
 		
-		if(strcmp(raw_input, "exit") == 0) {
+		if(strncmp(raw_input, "exit", 4) == 0) {
 			should_run = 0;
 			exit(0);
+		} else if(strncmp(raw_input, "history", 7) == 0) {
+			// print options
+			for(int i=0; i<history_count; i++) {// print command history
+
+			}
+			// scan in int
+			//
+			// error check
+			//
+			// run command
 		} else {
-			execute(raw_input);
+			execute(raw_input, &should_run);
+			update_command_history(tokens_history, raw_input);
+			if(history_count < 10) {
+				history_count++;
+			}
 		}
 	}
 	return 0;
 }
 
-char **tokenize(char *line){
+char **tokenize(char *line, int *is_background){
 	char *token;
 	char **tokens = malloc(MAX_LINE * sizeof(char*));
 	int index = 0;
@@ -51,23 +69,101 @@ char **tokenize(char *line){
 		return NULL;
 	}
 
+
 	// Parse Multicommands
-	if(strchr(line, ';') != NULL){
-		for (char *token = strtok(line, ';'); token != NULL; token = strtok(NULL, ';')){		
+	if(strchr(line, '>') != NULL || strchr(line, '<') != NULL) { // don't allow pipes to files
+		printf("Writing to file is not allowed\n");
+	} else if(strchr(line, ';') != NULL){
+		char *token = strtok(line, ";\n\r");
+		while(token != NULL) {
 			tokens[index] = token;
 			index++;
+			token = strtok(NULL, ";\n\r");
 		}
 
-		if(index >= MAX_LINE)
-		
-	} else { // Individual Command
-		for (char *token = strtok(line, ' '); token != NULL; token = strtok(NULL, ' ')){
-			tokens[index] = token;
-			index++;
+		if(index >= MAX_LINE){
+			printf("Input too large\n");
 		}
+		
+	} else if(strchr(line, '|') != NULL) { // handle pipes
+		// don't allow background pipes
+		if(strchr(line, '&') != NULL){
+			printf("Pipes are not allowed in the background\n");
+		}
+
+	} else { // Individual Command
+		char *token =  strtok(line, " \n\r");
+		while(token != NULL){
+			tokens[index] = token;
+			printf("%s\n", tokens[index]);
+			index++;
+			token = strtok(NULL, " \n\r");
+		}
+
+		if(index >= MAX_LINE){
+			printf("Input too large\n");
+		}// in case they have some massive input
+		
+		// & is last character of last token (even if there is a space before if making it its own token
+		if(*(tokens[index - 1] + strlen( tokens[index - 1]) - sizeof(char)) == '&') {
+			//printf("& found\n");
+			*is_background = 1;
+		}
+
+		tokens[index] = NULL;
 	}
+
+	return tokens;
 }
 
-void execute(char *raw_in){
+void execute(char *raw_in, int *should_run){
+	//int totalTokens = 0;
+	int is_background = 0;
+	char **tokens = tokenize(raw_in, &is_background);
+	//printf("%d", is_background);
+	//check if null, send error
+		
+	if(!is_background) {// non background task
+		int child_pid = fork();
+		if(child_pid == 0){// child process
+			//printf("Executing %s", tokens[0]);// debugging line
+			execvp(tokens[0], tokens);
+			printf("An error occured during execution, check your command format\n");
+			//*should_run = 0; //exit loop?
+		} else {// parent process
+			int status = -1;
+			waitpid(child_pid, &status, 0);
+			printf("%s exited with: %d\n", tokens[0], WEXITSTATUS(status));
+		}
+
+	} else { // background task
+		int child_spawner_pid = fork();
+		if(child_spawner_pid == 0) {// inside child_spawner_process
+			int background_process_pid = fork();
+			if(background_process_pid == 0) {
+				//printf("Executing %s", tokens[0]);// debugging line
+				execvp(tokens[0], tokens);
+				printf("An error occured during execution, check your command format\n");
+				//*should_run = 0;
+			} else {
+				int status = -1;
+				waitpid(background_process_pid, &status, 0);
+				printf("%s exited with: %d\n", tokens[0], WEXITSTATUS(status));
+			}
+	
+		}
+
+	}
+
+	// run it	
+}
+
+void update_command_history(char tokens_history[][MAX_LINE], char *last_command) {
 	
 }
+
+
+
+
+
+
