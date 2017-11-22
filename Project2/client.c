bb3752eeccf6f0cb6bbe4cb0ce71690df4235463
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-11-21 20:36:24 -0600

    Start of Project 2

---
+++ Project2/client.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main(int arg, char *argv[]) {
+	struct sockaddr_in address;
+	int sock = 0, valread;
+	struct sockaddr_in serv_addr;
+
+	if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Socket could not be created \n");
+		return -1;
+	}
+
+	memset(&serv_addr, '0', sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+		printf("Invalid address\n");
+		return -1;
+	}
+
+	if(connect(sock, (struct sockaddr * )&serv_addr, sizeof(serv_addr)) < 0){
+		printf("Connection could not be established\n");
+		return -1;
+	}
+
+	char message[1024];
+	char response[1024];
+
+	printf("Please provide message less than 1023 characters\n");
+	fgets(message, sizeof(message), stdin);
+
+	send(sock, message, strlen(message), 1024);
+	printf("Message send\n");
+	valread = read(sock, response, 1024);
+	printf("%s\n", response);
+	return 0;
+}
